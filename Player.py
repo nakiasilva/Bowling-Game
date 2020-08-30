51a91519bba22e34a9662850dec7b19905a2cbc6
Nakia <nakia.silva24@gmail.com>
2020-08-30 19:42:00 +1000

    Adding Player class

---
+++ Player.py
@@ -0,0 +1,13 @@
+import BowlingGame
+
+
+class Player:
+
+    def __init__(self, name, bowlingGame = None): 
+        self.player_name = name 
+        self.score = 0
+        self.bowlingGame = bowlingGame
+
+
+
+
